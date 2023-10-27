5a58a9dca4bf1d9c0d40a7117bba7092ee398088
H-Ayoobi <hamed_ayoobi@yahoo.com>
2023-10-27 17:23:03 +0100

    path updates

--- utils/Config.py
+++ utils/Config.py
@@ -1,6 +1,6 @@
 
 class Config(object):
-    pretrained_path = '/data2/hay/resnet_finetune_cub/se_base_model/model_pretraiend'
+    pretrained_path = '/data2/hay/resnet_finetune_cub/'
     expansion = 4
-    data_path = '/data2/hay/resnet_finetune_cub/fine-grained/cub200'
-    model_save_path = '/data2/hay/resnet_finetune_cub/model_save'
+    data_path = '/data2/CUB200'
+    model_save_path = '/data2/hay/model_save'
